2ae062e5fbc97991a134214bc5042eed152c5266
AnRin05 <phoebekirsten05@gmail.com>
2024-11-06 13:37:03 +0800

    Delete AppFB directory

--- AppFB/MainPage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='bl-home'),
-    path('about/', views.about, name='bl-about'),
-]
